f42db41806704f5516a0cc1ad6d400e08f09c37f
hwangjb <qlghd98@gmail.com>
2024-03-13 16:59:49 +0900

    mf Fibonacci number
    [+] 20240313
    
    반복문을 사용해서 n번째 피보나치 수를 구하는 것으로 변경

--- test_fibonacci_numbers/src/main.rs
+++ test_fibonacci_numbers/src/main.rs
@@ -10,13 +10,27 @@ fn main() {
 
     let n: u32 = input.trim().parse().expect("Please type a number!");
 
-    println!("The fibonacci numbers are: {}", fibonacci(n));
+    let mut a: u64 = 1;
+    let mut b: u64 = 1;
+    let mut c: u64 = 0;
+    for i in 1..n + 1 {
+        if i == 1 || i == 2 {
+            //println!("The fibonacci number {} is: 1", i);
+        } else {
+            c = a + b;
+            //println!("The fibonacci number {} is: {}", i, c);
+            a = b;
+            b = c;
+        }
+    }
+    //println!("The fibonacci numbers are: {}", fibonacci(n));
+    println!("The fibonacci numbers are: {}", c);
 }
 
-fn fibonacci(n: u32) ->  u64{
-    if n == 1 || n == 2 {
-        1
-    } else {
-        fibonacci(n - 1) + fibonacci(n - 2)
-    }
-}
+// fn fibonacci(n: u32) -> u64 {
+//     if n == 1 || n == 2 {
+//         1
+//     } else {
+//         fibonacci(n - 1) + fibonacci(n - 2)
+//     }
+// }
