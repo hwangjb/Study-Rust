fd3d3bb6958753ce253e13f6cd486a418b2d548c
hwangjb <qlghd98@gmail.com>
2024-03-15 17:28:40 +0900

    Study References, Borrowing
    [+] 20240315

---
+++ references_borrowing/src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    let s1 = String::from("hello");
+
+    let len = calculate_length(&s1);
+
+    println!("The length of '{}' is {}.", s1, len);
+
+    let mut s = String::from("hello");
+    change(&mut s);
+    println!("The length of '{}' is {}", s, calculate_length(&s));
+}
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
+}
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
+}
